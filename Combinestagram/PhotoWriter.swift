46c151ffa2f49ee92253f924bcfcbe0b40ff52a0
eng-oday <odaydieg@gmail.com>
2023-12-14 09:08:42 +0200

    solve challenge 1 , convert custom observable to single observable 🌪️

--- Combinestagram/PhotoWriter.swift
+++ Combinestagram/PhotoWriter.swift
@@ -10,8 +10,9 @@ class PhotoWriter {
     case couldNotSavePhoto
   }
 
-  static func save(_ image:UIImage) -> Observable<String> {
-    return Observable.create ({ observer in
+  static func save(_ image:UIImage) -> Single<String> {
+    
+    return Single.create { observer in
       var savedAssetId:String?
      
         
@@ -22,16 +23,16 @@ class PhotoWriter {
         } completionHandler: { success, error in
           DispatchQueue.main.async {
             if success , let id = savedAssetId{
-              observer.onNext(id)
-              observer.onCompleted()
+
+              observer(.success(id))
             }else {
-              observer.onError(error ?? Errors.couldNotSavePhoto)
+              observer(.failure(error ?? Errors.couldNotSavePhoto))
             }
           }
         }
 
       return Disposables.create()
-    })
+    }
   }
 
 }
